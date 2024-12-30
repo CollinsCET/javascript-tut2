406429e3c9d5c4522e911ed5bb538ed5f86628dd
Collins <ugwuamadicolins@gmail.com>
2024-12-30 20:16:07 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,81 @@
+// let priceOne = 20
+// let priceTwo = 100
+
+// priceTwo = 500
+
+// let result = priceOne + priceTwo 
+
+
+// console.log(result); 
+
+// let name
+// name = "collins"
+// console.log(name);
+
+// Increment Operator
+// let numberOfCartItem = 10
+// numberOfCartItem += 50
+
+// console.log(numberOfCartItem);
+
+// // numberOfCartItem++
+
+// let age = "70"
+// age = Number(age)
+// console.log(typeof(age));
+
+// let age = prompt("How old are you")
+// age = Number(age);
+
+// if (age >= 40){
+//     console.log("You are not alllowed to the party");
+// }
+// else{
+//     console.log("You are welcome to this party");
+// }
+
+// let totalPrice = 700
+// let yourName = "Collins"
+// let invoice = `Hello ${yourName}, your total payment is ${totalPrice} Naira. `
+
+// console.log(invoice);
+
+// let number = 5.9
+// let outcome= Math.round(number)
+// console.log(outcome);
+
+// let randonNumber = Math.random()
+// console.log(randonNumber)
+
+// let studentGrade = 70
+// let minimumScholarshipScore = 75
+// let grantScore = 70
+
+// if (studentGrade > minimumScholarshipScore) {
+//     alert( "Congratulations! you have Qualified")
+// }else if (studentGrade === grantScore) {
+//     alert( "You qualify for the Grant")
+// } else {
+//     alert("Not Passed, check next time")
+// }
+
+
+let josephCar = {
+    color: "Blue",
+    model: "Jo22uj929",
+    plateNumber: "Xykk809"
+
+}
+
+console.log(josephCar);
+
+
+
+
+
+
+
+
+
+
+
